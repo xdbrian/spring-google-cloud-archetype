d68a50d27bd7a9cf9543b140e4cba20147170dd7
Brian Steventh Torres Yañez <xdbrian.steventh@gmail.com>
2019-08-13 03:18:45 -0500

    Habilita KPI

--- src/main/java/com/example/demo/service/impl/ServiceAnalyzeImpl.java
+++ src/main/java/com/example/demo/service/impl/ServiceAnalyzeImpl.java
@@ -1,17 +1,20 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.ClientResponse;
 import com.example.demo.model.KpiClienteResponse;
 import com.example.demo.service.ServiceAnalyze;
 import com.example.demo.service.ServiceClient;
+import com.example.demo.util.AnalyzeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//import review.intercorp.client.external.model.ApiExpectativeLife;
-//import review.intercorp.client.external.model.ExpectativeLifeResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
  * Getión de métricas.
+ *
  * @Autor xdbrian.steventh@gmai.com
  */
 @Service
@@ -27,15 +30,15 @@ public class ServiceAnalyzeImpl implements ServiceAnalyze {
     public KpiClienteResponse kpideclientes() {
         KpiClienteResponse kpiClienteResponse = new KpiClienteResponse();
 
-//        List<Integer> listAge = serviceClient.list().stream()
-//                .map(ClientResponse::getEdad)
-//                .collect(Collectors.toList());
-//
-//        Float average = AnalyzeUtil.average.apply(listAge);
-//        kpiClienteResponse.setAverage(average);
-//
-//        List<Integer> listAges = serviceClient.list().stream().map(ClientResponse::getEdad).collect(Collectors.toList());
-//        kpiClienteResponse.setStandardDesviation(AnalyzeUtil.desviationStandard.apply(listAges));
+        List<Integer> listAge = serviceClient.list().stream()
+                .map(ClientResponse::getEdad)
+                .collect(Collectors.toList());
+
+        Float average = AnalyzeUtil.average.apply(listAge);
+        kpiClienteResponse.setAverage(average);
+
+        List<Integer> listAges = serviceClient.list().stream().map(ClientResponse::getEdad).collect(Collectors.toList());
+        kpiClienteResponse.setStandardDesviation(AnalyzeUtil.desviationStandard.apply(listAges));
         return kpiClienteResponse;
     }
 
