830f4537d54f8edd2b18bb7e9fb10708b1645132
Brian Steventh Torres Yañez <xdbrian.steventh@gmail.com>
2019-08-13 20:38:01 -0500

    test-fix: deploy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({
+		"com.example.demo",
+		"com.example.demo.config",
+		"com.example.demo.expose",
+		"com.example.demo.external",
+		"com.example.demo.model",
+		"com.example.demo.service",
+		"com.example.demo.impl",
+})
 @SpringBootApplication
 public class DemoApplication {
 
